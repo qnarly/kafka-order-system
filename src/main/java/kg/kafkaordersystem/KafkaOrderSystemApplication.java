c697048c36d15d937d766c3a55e9bb3fa221c675
tursunovAlihan <alihantursunov606@gmail.com>
2026-01-04 22:24:09 +0600

    created project

---
+++ src/main/java/kg/kafkaordersystem/KafkaOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package kg.kafkaordersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaOrderSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/kafkaordersystem/KafkaOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kafkaordersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaOrderSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
