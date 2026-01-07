8bd41f7409899f509c1b4564dd0dc334e2a7090b
tursunovAlihan <alihantursunov606@gmail.com>
2026-01-07 13:38:43 +0600

    add LoanApplication entity with fields for client details and application status

---
+++ producer-service/src/main/java/kg/producerservice/model/LoanApplication.java
@@ -0,0 +1,37 @@
+package kg.producerservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "loan_applications")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoanApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "client_name", nullable = false)
+    private String clientName;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private BigDecimal salary;
+
+    @Column(nullable = false)
+    private String status;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+}
